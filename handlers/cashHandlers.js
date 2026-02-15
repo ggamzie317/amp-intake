eac449a090601fa7acb3d819bacab12782f382be
ggamzie317 <ggamzie317@gmail.com>
2026-02-15 17:23:31 +0800

    Update cashHandlers.js

--- handlers/cashHandlers.js
+++ handlers/cashHandlers.js
@@ -1,9 +1,7 @@
 /**
  * 현금/금융 관련 비즈니스 로직 핸들러
  */
-import { buildResponse } from './_response.js';
 const cashHandlers = {
-  // [P01] 현금화 vs 전환 분석
   'M01_CASH_OUT_VS_TRANSFER_ANALYSIS': async (data) => {
     const ratio = data.domain?.pricing?.transferRatio || 1.0;
     const balance = data.domain?.balances?.pointBalance || 0;
@@ -13,7 +11,6 @@ const cashHandlers = {
       valueEstimate: balance * ratio
     };
   }
-  // 이후 M02, M03, M04, M05 등을 여기에 추가하세요.
 };
 
 module.exports = cashHandlers;
